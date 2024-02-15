e1febc4d94733936a7125b6b31b3c32801cb545f
Ryan Pillay <ryan.pillay@rank.com>
2024-02-15 13:09:16 +0200

    testing port changes for hosting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const session = require("express-session");
 const bcrypt = require("bcrypt");
 const app = express();
-const port = 80;
+const port = 443;
 // Import the path module
 const path = require("path");
 const sqlite3 = require("sqlite3").verbose();
