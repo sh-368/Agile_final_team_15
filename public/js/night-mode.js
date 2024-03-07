c5dfa96409625405abf534f0993798525a4ae7fc
Ryan Pillay <ryan.pillay@rank.com>
2024-03-07 21:21:14 +0200

    card sections night mode fix

--- public/js/night-mode.js
+++ public/js/night-mode.js
@@ -1,29 +1,54 @@
 let isNightMode = false;
 
 const nightModeToggle = document.getElementById('nightModeToggle');
-const pageSections = document.querySelectorAll('.page-section');
+const allLatestSection = document.querySelectorAll('.latest-section');
+const allCards = document.querySelectorAll('.card');
+const navBar = document.getElementById('mainNav');
 
 nightModeToggle.addEventListener('click', () => {
+
     isNightMode = !isNightMode;
+
     if (isNightMode) {
-        nightModeToggle.innerHTML = '<i class="fas fa-moon fa-2xl text-dark"></i>'; // Switch to moon icon for night mode
+        nightModeToggle.innerHTML = '<i class="fas fa-moon fa-2xl text-light"></i>'; // Switch to moon icon for night mode
         document.body.style.backgroundColor = '#333333'; // Change background color for night mode
         document.body.style.color = '#fff'; // Change text color for night mode
-const pageSections = document.querySelectorAll('.page-section');
-        console.log(pageSections)
-        pageSections.forEach(pageSection => {
-            pageSection.classList.remove('section-day');
-            pageSection.classList.add('section-night');
+    
+        navBar.classList.remove('navbar');
+        navBar.classList.add('navbar-night');
+
+        console.log(allLatestSection)
+        allLatestSection.forEach(latestSection => {
+            latestSection.classList.remove('latest-section');
+            latestSection.classList.add('latest-section-night');
+
+        })
+
+        allCards.forEach(card => {
+            card.style.backgroundColor = '#333333';
+            card.classList.remove('bg-white p-3');
+            card.classList.add('bg-secondary p-3');
+
         })
 
     } else {
+
         nightModeToggle.innerHTML = '<i class="fas fa-sun fa-2xl text-light"></i>'; // Switch to sun icon for day mode
         document.body.style.backgroundColor = '#fff'; // Change background color for day mode
         document.body.style.color = '#000'; // Change text color for day mode
-        console.log(pageSections)
-        pageSections.forEach(pageSection => {
-            pageSection.classList.remove('section-night');
-            pageSection.classList.add('section-day');
+
+        allLatestSection.forEach(latestSection => {
+            latestSection.classList.add('latest-section');
+            latestSection.classList.remove('latest-section-night');
         })
+
+        navBar.classList.remove('navbar-night');
+        navBar.classList.add('navbar');
     }
+
+    allCards.forEach(card => {
+        card.classList.remove('bg-secondary p-3');
+        card.classList.add('bg-white p-3');
+
+    })
 });
